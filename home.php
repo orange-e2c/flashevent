c1059757bfd9429604ce4cf4f509407f21c9fb47
Moreau Aurelien <aurelmoreau@outlook.fr>
2016-05-20 14:17:36 +0000

    merge

--- home.php
+++ home.php
@@ -1,25 +1,41 @@
-<!-- Tab panes -->
-<h1 class="accroche">Venez dansez au pied de la Tour Part Dieu, le 21 Juin prochain !</h1>
-	
-<div class="row">
-	<div id="banderole band">
-		<div class="col-lg-offset-2 col-lg-8 col-lg-offset-2">	
-			<img src="assets/images/banderole.png" alt="partdieu" class="img-thumbnail">
-		</div>
-		<div id="titre-banderole"   style="center">
-						<h1 style=";text-color:white;" >
-							LA TOUR CRAYON</br>LA PART DIEU
-						</h1>
-		</div>
-	</div>
-</div>
-				
-			
-<div id="titre" style="background:yellow;">
-</div>
-				<!--possible mise en page-->
-		<div id="video">
-			<div id="container_video" >
-				<iframe width="95%" height="720" src="https://www.youtube.com/embed/95NjjrXzhak" frameborder="0" allowfullscreen></iframe>
+<div class="container-fluid">
+
+	<div class="row">
+		<div class="col-sm-offset-1 col-sm-10">
+			<div class="jumbotron">
+				<h2>Venez dansez au pied de la Tour Part Dieu !</h2>
 			</div>
 		</div>
+	</div>
+	
+	<div class="clearfix" style="height:40px"></div>
+
+	<div class="row">
+		<div class="col-sm-offset-1 col-sm-10">
+			
+		<div class="media">
+		  <div class="media-left">
+		    <a href="#">
+		      <img class="media-object" src="assets/images/banderole.png" style="width: 128px;height: 128px;" alt="partdieu">
+		    </a>
+		  </div>
+		  <div class="media-body">
+		    <h4 class="media-heading">le 21 Juin prochain</h4>
+			<p>
+Proin lacinia blandit nunc. Ut at metus dictum, faucibus dolor et, bibendum libero. In quis lacinia risus, vitae elementum nisl. Etiam nec tincidunt purus. Aenean malesuada aliquam mauris vitae pellentesque. Maecenas a tortor volutpat, sagittis ipsum quis, volutpat lacus. Proin sit amet leo in lorem malesuada porttitor ut quis dolor. Vestibulum ante lectus, elementum a nibh at, rhoncus semper enim. Nam nec ligula erat. Donec tincidunt elit sit amet lacus consequat, in luctus ipsum ornare. Ut pulvinar congue viverra. Sed ipsum sem, fringilla in orci sit amet, eleifend vestibulum est. Nunc tempor, purus vitae porttitor scelerisque, ante ipsum pellentesque turpis, non volutpat dolor metus id sapien. Proin laoreet bibendum semper. Integer vehicula rhoncus faucibus. Nulla in urna condimentum massa ultrices luctus at vitae lacus.
+
+			</p>
+		  </div>
+		</div>			
+		</div>
+	</div>
+
+	<div class="clearfix" style="height:50px"></div>
+
+	<div class="row">
+		<div class="col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6" style="height:300px">
+			<iframe class="col-sm-12" width="100%" height="100%" src="https://www.youtube.com/embed/95NjjrXzhak" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+
+</div>
